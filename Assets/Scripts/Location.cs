f1cc7929ca95566fdad407fdd58efcca37cac974
paul tschirgi <pault1@umbc.edu>
2017-04-02 02:12:34 -0400

    Setup for drop down menu for state and city selection and manager that finds index from selected option out of list

---
+++ Assets/Scripts/Location.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class Location {
+
+    public int stateIndex;
+    public int cityIndex;
+
+    public int dateMonth;
+    public int dateDay;
+    public int dateYear;
+
+    //1 - 12
+    public int timeHour;
+    //0-59
+    public int timeMin;
+    //true = am, false = pm
+    public bool isAM;
+
+
+
+}
---
+++ Assets/Scripts/linkGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class linkGenerator : MonoBehaviour {
+
+    //This script has a reference to all set able values and converts it to a link string (?&..) 
+
+    public TruckData truckingData;
+
+
+    //References:
+    //PICK UP REFs
+    public tk2dUIDropDownMenu P_stateMenu;
+    public tk2dUIDropDownMenu P_cityMenu;
+
+
+
+
+    //PICK UP REFs
+    public tk2dUIDropDownMenu D_stateMenu;
+    public tk2dUIDropDownMenu D_cityMenu;
+
+
+
+    void MakeLink ()
+    {
+        Debug.Log("making link");
+
+        truckingData.pickUpInfo.stateIndex = (System.Array.IndexOf(P_stateMenu.startingItemList, P_stateMenu.selectedTextMesh.text));
+        truckingData.pickUpInfo.cityIndex = (System.Array.IndexOf(P_cityMenu.startingItemList, P_cityMenu.selectedTextMesh.text));
+
+
+
+
+        truckingData.dropOffInfo.stateIndex = (System.Array.IndexOf(D_stateMenu.startingItemList, D_stateMenu.selectedTextMesh.text));
+        truckingData.dropOffInfo.cityIndex = (System.Array.IndexOf(D_cityMenu.startingItemList, D_cityMenu.selectedTextMesh.text));
+    }
+
+}
