b15af7e36a29fe72f886852688bcc07b9d6d5172
Eliot Carney-Seim <ecsit39@gmail.com>
2017-04-02 06:55:54 -0400

    Updated to 5.6

---
+++ Assets/Scripts/ParseManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.VisualBasic.FileIO;
+
+public class ParseManager : MonoBehaviour {
+
+    TextFieldParser parser;
+
+	// Use this for initialization
+	void Start () {
+        parser = new TextFieldParser(@"c:\temp\test.csv");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
