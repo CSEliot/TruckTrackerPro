64d8953e9690ce371b6e9164c3031bae1d4aad16
paul tschirgi <pault1@umbc.edu>
2017-04-01 21:35:10 -0400

    Mostly just 2d toolkit plugin and truck

---
+++ Assets/TruckData.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+[Serializable]
+public class TruckData
+{
+    public int uniqueTruckID;
+    
+}
---
+++ Assets/WebDataTester.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+
+public class WebDataTester : MonoBehaviour {
+
+
+    public TruckData myData;
+
+    public tk2dTextMesh texty;
+
+    void Start()
+    {
+
+            LoadFiles();
+        
+        //if no file, create one?
+    }
+
+        // Use this for initialization
+    void SaveS ()
+    {
+       
+
+        myData.uniqueTruckID++;
+        texty.text = myData.uniqueTruckID.ToString();
+
+       // if (Application.platform == RuntimePlatform.WebGLPlayer)
+       // {
+
+
+       SaveFiles();
+        
+    }
+	
+    void LoadFiles()
+    {
+        
+        myData = SaveLoadData.Load();
+        texty.text = myData.uniqueTruckID.ToString();
+    }
+
+    void SaveFiles()
+    {
+        SaveLoadData.Save(myData);
+    }
+}
